0e61ad11a1a17c03ba406cca63f479350dda14d4
DuganChandler <duganchandler10@gmail.com>
2025-02-13 13:03:46 -0500

    Base Encounter code created. Will need to be adjusted

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,25 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour {
     [SerializeField] private float walkSpeed = 10f;
 
-    Vector2 moveInput;
-    Rigidbody rb;
+    [Header("Encounter Settings")]
+    [SerializeField] private LayerMask encounterLayer;
+    [SerializeField] private float encounterChance = 0.3f;
+    [SerializeField] private float distanceThreshhold = 10f;
+
+    private Vector3 lastPosition;
+    private float distanceAccumulated = 0f;
+
+    private Vector2 moveInput;
+    private Rigidbody rb;
+    private CapsuleCollider capsuleCollider;
+
+
 
     void Start() {
         rb = GetComponent<Rigidbody>();
-        
+        capsuleCollider = GetComponent<CapsuleCollider>(); 
+
+        lastPosition = transform.position;
     }
 
     void Update() {
-        
+        Vector3 currentPos = transform.position;
+        float distanceMoved = Vector3.Distance(currentPos, lastPosition);
+        distanceAccumulated += distanceMoved;
+        lastPosition = currentPos;
+
+        if (IsEncounterLayer()) {
+            if (distanceAccumulated >= distanceThreshhold) {
+                if (Random.value <= encounterChance) {
+                    Debug.Log("RANDOM ENCOUNTER");
+                }
+                distanceAccumulated = 0f;
+            }
+        } else {
+            distanceAccumulated = 0f;
+        } 
     }
 
     void FixedUpdate() {
        Run(); 
+    }
+
+    private bool IsEncounterLayer() {
+        RaycastHit hit;
+
+        Debug.DrawRay(capsuleCollider.bounds.center, Vector3.down * 1f, Color.red);
+        if (Physics.Raycast(capsuleCollider.bounds.center, Vector3.down, out hit, 1f, encounterLayer)) {
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckRandomEncounter() {
+        if (!IsEncounterLayer()) {
+            return;
+        }
+
     }
 
     void Run() {
@@ -31,5 +76,10 @@ public class PlayerController : MonoBehaviour {
     public void onMove(InputAction.CallbackContext context) {
         moveInput = context.ReadValue<Vector2>();
     }
-    
+
+    void OGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * 0.1f);
+    }
+
 }
