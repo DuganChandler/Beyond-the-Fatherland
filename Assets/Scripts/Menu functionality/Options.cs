1a84be9afd603693b12d0ff95e182a0296fd3a32
AvocadoDisaster <and80800@gmail.com>
2025-03-17 22:31:49 -0400

    Menu functionality

---
+++ Assets/Scripts/Menu functionality/Options.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEditor.Progress;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Options : MonoBehaviour
+{   
+    public GameObject optionsMenuUI;
+    public GameObject StartingButton;
+    public bool optionOpen = true;
+    public Mainmenu Mainmenu;
+    public PauseMenu PauseMenu;
+
+    public void openOpps()
+    {
+        
+        optionsMenuUI.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(StartingButton);
+        Debug.Log("The options Menue is open");
+        optionOpen = true;
+
+    }
+
+    public void stopOpps()
+    {
+        optionsMenuUI.SetActive(false); ;
+        Debug.Log("The options Menue is closed");
+        optionOpen = false;
+        if (PauseMenu.IsGamePaused == true)
+        {
+            EventSystem.current.SetSelectedGameObject(PauseMenu.firseButton);
+        }
+        else { EventSystem.current.SetSelectedGameObject(Mainmenu.StartinngButton); }
+        
+    }
+
+    public void volume ()
+    {
+        Debug.Log("Volume is volumeing");
+    }
+
+
+
+}
